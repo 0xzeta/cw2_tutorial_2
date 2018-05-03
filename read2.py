c2139199c07bcfdef39a0ddd27338dce93d6f67d
Napier40401620 <zhtun408@gmail.com>
2018-05-03 23:39:26 +0630

    fix problem of read2 tutorial

--- read2.py
+++ read2.py
@@ -1,17 +1,17 @@
 import urllib2,json,base64
 accesstoken="GBSR8NESEMWDEA15929E"
 institution="10007772"
-page=0
-url="http://data.unistats.ac.uk/api/v4/KIS/Institution/{}/Courses.json?pageIndex={}".format(
+page=3
+url="http://data.unistats.ac.uk/api/v4/KIS/Institution/{}/Courses.json?pageIndex={}/statics2".format(
 	institution,
 	page
 	)
 request=urllib2.Request(url)
 request.add_header(
 	"Authorization",
-	"Basic" + base64.encodestring(accesstoken+":").replace('\n','')
+	"Basic " + base64.encodestring(accesstoken+":").replace('\n','')
 	)
 response=urllib2.urlopen(request)
 data=json.load(response)
 for c in data:
-	print c["KisCourseID"], c["Title"]
+	print c["KisCourseId"], c["Title"]
